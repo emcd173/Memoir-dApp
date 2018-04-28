5cf4c57714eaaf67771cd3c8f00a4c64b21916e9
Skylar Weaver <sweaver@andrew.cmu.edu>
2018-04-28 18:21:20 -0400

    Contract from master

--- client/contracts/Amsterdam.sol
+++ client/contracts/Amsterdam.sol
@@ -1,15 +1,6 @@
 pragma solidity ^0.4.19;
 
-contract ownable {
-    address owner;
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
-    }
-    function ownable() public {
-        owner = msg.sender;
-    }
-}
+import "./ownable.sol";
 
 contract Amsterdam is ownable {
 
@@ -21,11 +12,13 @@ contract Amsterdam is ownable {
     string ipfs;
     string title;
     string descrip;
+    uint entryType;
+    uint[] enc_msg;
   }
-  
+
   struct PK {
       uint id;
-      string key;
+      uint[] key;
   }
 
   // state variables
@@ -36,73 +29,129 @@ contract Amsterdam is ownable {
   // Event declaration
   event EvtRelease(
     address indexed _owner,
-    string _key,
+    uint[] _key,
     string _ipfs
-  );
+    );
 
-  event EvtEntryAppended(
-    uint indexed id
-  );
-    
+  event EventEncMsg(
+    uint[] _enc_msg
+    );
+
+  event EventDecMsg(
+    uint[] _dec_msg
+    );
+
   function Amsterdam() public {
   }
 
   // add an entry to the blockchain
-  function appendEntry(uint _unlockTime, string _ipfs, string _title, string _description, string _file) public {
+  function appendEntry(uint _unlockTime, string _ipfs, string _title, string _description, uint _entryType, uint[] _file, uint _rand) public {
     // a new entry
     counter++;
+   // adding info to Entry struct
+   entries[counter] = Entry(
+       counter,
+       _unlockTime + block.timestamp,
+       msg.sender,
+       _ipfs,
+       _title,
+       _description,
+       _entryType,
+       encrypt(counter, _file, _rand)
+       );
 
-    // adding info to Entry struct
-    entries[counter] = Entry(
-        counter,
-        _unlockTime,
-        msg.sender,
-        _ipfs,
-        _title,
-        _description
-        );
-        
-    // encrypt the file and return the private key 
-   encryptEntry(counter, _file);
-
-   EvtEntryAppended(counter);
-   
+    privateKeys[counter] = PK(
+      counter,
+      get_key(counter, _file)
+      );
   }
-  
-  function encryptEntry(uint _id, string _file) private {
-      // magic
-      privateKeys[_id] = PK(
-          _id,
-          "private key"
-          );
+
+  function encrypt(uint _id, uint[] _message, uint _rand) private returns(uint[]) {
+        uint[] memory mssg = _message;
+        uint len = mssg.length;
+        uint[] memory enc_msg = new uint[](len);
+        privateKeys[_id].key = new uint[](len);
+
+        for (uint i = 0; i < len; i++) {
+            privateKeys[_id].key[i] = _rand % 26;
+            if (mssg[i] >= 65 && mssg[i] <= 90)
+                enc_msg[i] =  (mssg[i] - 65 + privateKeys[_id].key[i]) % 26;
+            else if (mssg[i] >= 97 && mssg[i] <= 122)
+                enc_msg[i] =  (mssg[i] - 97 + privateKeys[_id].key[i]) % 26;
+            else
+                enc_msg[i] = mssg[i];
+        }
+
+        // tigger the event
+        EventEncMsg(entries[_id].enc_msg);
+
+        return(enc_msg);
+
     }
-  
-  function release(uint _id) public {
+
+  function release(uint _id) public{
     // check if it is time to release
     require(now >= entries[_id].unlockTime);
-    // trigger an event
+    decrypt(_id);
     EvtRelease(entries[_id].owner, privateKeys[_id].key, entries[_id].ipfs);
   }
 
-  function eventTest(uint _id) public {
+  function get_key(uint _id, uint[] _message) private view returns(uint[]){
+
+          uint len = _message.length;
+          uint[] memory temp = new uint[](len);
+
+          for (uint i = 0; i < len; i++) {
+              temp[i] = privateKeys[_id].key[i] + 65;
+          }
+          return temp;
+      }
+
+  function decrypt(uint _id) private returns(uint[]){
+    uint len = entries[_id].enc_msg.length;
+    uint[] memory dec_msg = new uint[](len);
+          for (uint i = 0; i < len; i++) {
+              if ((entries[_id].enc_msg[i] - privateKeys[_id].key[i]) < 0)
+                  dec_msg[i] =  entries[_id].enc_msg[i] - privateKeys[_id].key[i] + 26;
+              else if ((entries[_id].enc_msg[i] - privateKeys[_id].key[i]) >= 0)
+                  dec_msg[i] =  entries[_id].enc_msg[i] - privateKeys[_id].key[i];
+              else
+                  dec_msg[i] =  entries[_id].enc_msg[i];
+          }
+          // trigger the event
+          EventDecMsg(dec_msg);
+
+          return(dec_msg);
+      }
+
+
+  /* function eventTestRelease(uint _id) public {
     EvtRelease(entries[_id].owner, privateKeys[_id].key, entries[_id].ipfs);
   }
+
+  function eventTestEnc(uint[] _enc_msg) public {
+    EventEncMsg(_enc_msg);
+  }
+
+  function eventTestDec(uint[] _dec_msg) public {
+    EventDecMsg(_dec_msg);
+  } */
 
 
   // some getter functions
-  
+
   function getIPFS(uint _id) constant public returns (string x) {
       return entries[_id].ipfs;
   }
-  
-  function getTotalEnteries() public view returns (uint x){
+
+  function getTotalentries() public view returns (uint x){
       return counter;
   }
-  
+
   function getDescrip(uint _id) constant public returns (string x) {
     return entries[_id].descrip;
   }
-  
+
   function getTitle(uint _id) constant public returns (string x) {
       return entries[_id].title;
   }
